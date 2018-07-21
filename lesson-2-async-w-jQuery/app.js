bcfdc8b8ee9ae62295ba5f31973fd4914eb2a70a
viraj_lad <viraj.lad@gmail.com>
2018-07-21 23:01:03 +0000

    Started fetching images & articles using jQuery ajax method

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,36 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        
+        function addImage(images) {
+            const firstImage = images.results[0];
+            
+            responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+                    <img src="${firstImage.urls.regular}" alt="${searchedForText}" >
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                </figure>`);
+        }
+        
+        function addArticles(articles) {
+            responseContainer.insertAdjacentHTML('beforeend', '<ul>' +
+                    articles.response.docs.map( article => {
+                        return `<li class="article"> 
+                            <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                            <p>${article.snippet}</p>
+                        </li>`;
+                    }).join('') 
+                    + '</ul>');
+        }
+        
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID e773feaa9e6d9ebeb0b31622f72dcb785e89d6c2ff694059825416615d948b3c'
+            }
+        }).done(addImage);
+        
+        $.ajax({
+            url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=d0555377b67b44119cff7b28b808e0e2`
+        }).done(addArticles);
     });
 })();
